3faa2cdd7b2de71fbb3a4d80b4f48b92e2e35466
Garcia, Jenaro (J.) <jgarc609@ford.com>
2025-02-07 08:21:07 +0000

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+dependencies {
+    implementation("com.google.cloud.functions:functions-framework:2.10.0") // Or latest version
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("com.example.helloworld.MainKt") // Replace with your main class
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package org.example
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.println
+
+fun main() {
+    println("Hello World!")
+}
